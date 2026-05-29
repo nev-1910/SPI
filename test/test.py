d716b26552c933e12fcb207e88b18fa45f145c48
nev-1910 <nevinphilip10@gmail.com>
2026-05-29 15:26:50 +0530

    Updated SPI cocotb test

--- test/test.py
+++ test/test.py
@@ -1,40 +1,29 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import Timer
 
 
 @cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
+async def test_spi_master(dut):
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
+    cocotb.start_soon(Clock(dut.clk, 10, unit="ns").start())
 
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    dut.ena.value = 1
+
+    dut.ui_in.value = 0b10110011
+    dut.uio_in.value = 0
+
+    await Timer(20, unit="ns")
+
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    await Timer(20, unit="ns")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    dut.uio_in.value = 1
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    await Timer(20, unit="ns")
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    dut.uio_in.value = 0
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    await Timer(500, unit="ns")
