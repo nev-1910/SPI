1be7fd49b5d1a49ab267a9cb9382e3f0df917bc7
nev-1910 <nevinphilip10@gmail.com>
2026-05-31 20:00:58 +0530

    Refactor FSM and add shift registers for TX/RX

--- src/project.v
+++ src/project.v
@@ -13,7 +13,6 @@ module tt_um_spi_master (
     input  wire rst_n
 );
 
-    // FSM States
     localparam IDLE     = 2'b00;
     localparam LOAD     = 2'b01;
     localparam TRANSFER = 2'b10;
@@ -21,7 +20,10 @@ module tt_um_spi_master (
 
     reg [1:0] state;
 
-    reg [7:0] shift_reg;
+    reg [7:0] tx_shift_reg;
+    reg [7:0] rx_shift_reg;
+    reg [7:0] received_data;
+
     reg [2:0] bit_count;
 
     reg mosi_reg;
@@ -29,83 +31,89 @@ module tt_um_spi_master (
     reg cs_reg;
     reg busy_reg;
 
+    wire miso;
+
+    assign miso = uio_in[1];
+
     always @(posedge clk or negedge rst_n) begin
 
         if (!rst_n) begin
 
-            state     <= IDLE;
-            shift_reg <= 8'b0;
+            state <= IDLE;
+
+            tx_shift_reg <= 8'b0;
+            rx_shift_reg <= 8'b0;
+            received_data <= 8'b0;
+
             bit_count <= 3'b0;
 
-            mosi_reg  <= 1'b0;
-            sclk_reg  <= 1'b0;
-            cs_reg    <= 1'b1;
-            busy_reg  <= 1'b0;
+            mosi_reg <= 1'b0;
+            sclk_reg <= 1'b0;
+            cs_reg <= 1'b1;
+            busy_reg <= 1'b0;
 
         end
-
         else begin
 
             case(state)
 
-                //--------------------------------------------------
-                IDLE:
-                //--------------------------------------------------
-                begin
+                IDLE: begin
 
-                    cs_reg   <= 1'b1;
+                    cs_reg <= 1'b1;
                     busy_reg <= 1'b0;
                     sclk_reg <= 1'b0;
 
-                    if(uio_in[0]) begin
+                    if(uio_in[0])
                         state <= LOAD;
-                    end
 
                 end
 
-                //--------------------------------------------------
-                LOAD:
-                //--------------------------------------------------
-                begin
+                LOAD: begin
 
-                    shift_reg <= ui_in;
+                    tx_shift_reg <= ui_in;
+                    rx_shift_reg <= 8'b0;
+
                     bit_count <= 3'b0;
 
-                    cs_reg    <= 1'b0;
-                    busy_reg  <= 1'b1;
+                    cs_reg <= 1'b0;
+                    busy_reg <= 1'b1;
 
                     state <= TRANSFER;
 
                 end
 
-                //--------------------------------------------------
-                TRANSFER:
-                //--------------------------------------------------
-                begin
+                TRANSFER: begin
 
                     sclk_reg <= ~sclk_reg;
 
                     if(sclk_reg == 1'b0) begin
 
-                        mosi_reg <= shift_reg[7];
+                        mosi_reg <= tx_shift_reg[7];
 
-                        shift_reg <= shift_reg << 1;
+                        tx_shift_reg <= tx_shift_reg << 1;
 
-                        if(bit_count == 3'd7)
+                        rx_shift_reg <= {rx_shift_reg[6:0], miso};
+
+                        if(bit_count == 3'd7) begin
+
+                            received_data <= {rx_shift_reg[6:0], miso};
+
                             state <= DONE;
-                        else
-                            bit_count <= bit_count + 1;
+
+                        end
+                        else begin
+
+                            bit_count <= bit_count + 1'b1;
+
+                        end
 
                     end
 
                 end
 
-                //--------------------------------------------------
-                DONE:
-                //--------------------------------------------------
-                begin
+                DONE: begin
 
-                    cs_reg   <= 1'b1;
+                    cs_reg <= 1'b1;
                     busy_reg <= 1'b0;
                     sclk_reg <= 1'b0;
 
@@ -119,17 +127,16 @@ module tt_um_spi_master (
 
     end
 
-    // Output Mapping
     assign uo_out[0] = mosi_reg;
     assign uo_out[1] = sclk_reg;
     assign uo_out[2] = cs_reg;
     assign uo_out[3] = busy_reg;
 
-    assign uo_out[7:4] = 4'b0000;
+    assign uo_out[7:4] = received_data[3:0];
 
     assign uio_out = 8'b0;
     assign uio_oe  = 8'b0;
 
-    wire _unused = &{ena, uio_in[7:1], 1'b0};
+    wire _unused = &{ena, uio_in[7:2], 1'b0};
 
 endmodule
