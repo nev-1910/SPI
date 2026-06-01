66e3d043b25a61517e8a5a06b78bf679d88faa15
nev-1910 <nevinphilip10@gmail.com>
2026-06-01 15:14:44 +0530

    Added SPI slave with TX/RX shift registers

--- src/spi_slave.v
+++ src/spi_slave.v
@@ -1,1 +1,61 @@
+module spi_slave(
 
+    input sclk,
+    input cs,
+
+    input mosi,
+
+    input [7:0] tx_data,
+
+    output reg miso,
+
+    output reg [7:0] rx_data
+
+);
+
+reg [7:0] tx_shift;
+reg [7:0] rx_shift;
+
+reg [2:0] bit_count;
+
+always @(negedge cs)
+begin
+
+    tx_shift <= tx_data;
+    rx_shift <= 8'b0;
+
+    bit_count <= 3'b0;
+
+end
+
+always @(posedge sclk)
+begin
+
+    if(!cs)
+    begin
+
+        miso <= tx_shift[7];
+
+        tx_shift <= tx_shift << 1;
+
+        rx_shift <= {rx_shift[6:0], mosi};
+
+        if(bit_count == 3'd7)
+        begin
+
+            rx_data <= {rx_shift[6:0], mosi};
+
+        end
+
+        else
+        begin
+
+            bit_count <= bit_count + 1'b1;
+
+        end
+
+    end
+
+end
+
+endmodule
